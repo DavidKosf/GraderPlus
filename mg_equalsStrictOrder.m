2789a2ca1219102e44a31261b823b273bd4a7fcb
David Kosfelder <dako1999@web.de>
2020-05-20 11:43:10 +0200

    Added mg_equalsStrictOrder

---
+++ mg_equalsStrictOrder.m
@@ -0,0 +1,17 @@
+function [pass, missing] = mg_equalsStrictOrder(varNames)
+    pass = true();
+    missing = [];
+    
+    for var = varNames
+        
+        try
+            fncCall = "assessVariableEqual('" + var + "', referenceVariables."+var+");"
+            evalin('caller', fncCall);
+        catch
+            pass = false();
+            missing = [missing, var];
+        end
+    end
+    
+end
+
